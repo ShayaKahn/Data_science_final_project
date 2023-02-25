84a070db62f06e86fe5bf33788138b92d285f8d2
shaya <shayakan9@gmail.com>
2023-02-25 22:42:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+os.chdir(r'C:\Users\shaya\OneDrive\Desktop\Data_science_project')
+
+# Load the data into a pandas DataFrame
+df = pd.read_csv('US_Accidents_Dec21_updated.csv')
+print(df)
+
+# The problem I'm trying to solve is classification, i.e, to 
+
